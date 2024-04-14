79fd59ea08c34068faade6e66998933584b6de12
srijan041 <gunzerker41@gmail.com>
2024-04-14 14:28:49 +0530

    added user register and bug fixes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,10 +3,12 @@ class ApiResponse {
         statusCode,
         data,
         message = "Success"
-    ){
+    ) {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
         this.success = statusCode < 400
     }
 }
+
+export { ApiResponse }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -24,4 +24,4 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadOnCloudinary }
+export { uploadOnCloudinary }
