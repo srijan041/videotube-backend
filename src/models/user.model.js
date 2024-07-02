d6f12377a133095967226c040fd114ded03ea388
srijan041 <gunzerker41@gmail.com>
2024-07-03 01:05:29 +0530

    renaming env variable

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -81,7 +81,7 @@ userSchema.methods.generateRefreshToken = function () {
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_SECRET
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
         }
     )
 }
