b95c8e116ff2bb5ce38d8b517d94c4edf3db9be9
srijan041 <gunzerker41@gmail.com>
2024-07-11 15:48:46 +0530

    Fixed likes on comments and tweets

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,8 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { Video } from "../models/video.model.js"
+import { Comment } from "../models/comment.model.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params
